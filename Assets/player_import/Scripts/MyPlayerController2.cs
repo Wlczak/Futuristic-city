ee8e63089923f8afbe80e8ae65f08489fde13de7
Adam Vlček <vlcek-adam@seznam.cz>
2024-10-16 18:47:58 +0200

    Update MyPlayerController2.cs

--- Assets/player_import/Scripts/MyPlayerController2.cs
+++ Assets/player_import/Scripts/MyPlayerController2.cs
@@ -1,17 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-// This script moves the character controller forward
-// and sideways based on the arrow keys.
-// It also jumps when pressing space.
-// Make sure to attach a character controller to the same game object.
-// It is recommended that you make only one call to Move or SimpleMove per frame.
-
 public class MyPlayerController2 : MonoBehaviour
 {
     CharacterController characterController;
 
-    public GameObject _camera;
+    public GameObject _camera;          // Player camera
+    public GameObject car;              // Car object to follow
     public float _speed = 6.0f;
     public float _runSpeed = 12.0f;    
     public float _jumpSpeed = 8.0f;
@@ -19,12 +14,18 @@ public class MyPlayerController2 : MonoBehaviour
     public float _gravity = 20.0f;
     public KeyCode _run;
     public KeyCode _flyDown;
+    public KeyCode switchKey = KeyCode.C;  // Key to switch between player and car follow
 
     private Vector3 moveDirection = Vector3.zero;
 
     // Player angles in degrees
     private float pitch;
     private float yaw;
+
+    private bool isFollowingCar = false;  // Tracks whether we're in car-following mode
+
+    // Camera offset for following the car
+    public Vector3 carCameraOffset = new Vector3(0, 5, -10);
 
     void Start()
     {
@@ -37,50 +38,73 @@ public class MyPlayerController2 : MonoBehaviour
 
     void Update()
     {
-        pitch = Mathf.Clamp(pitch - Input.GetAxis("Mouse Y") * _rotationSpeed, -90, 90);
+        // Check for camera switch input
+        if (Input.GetKeyDown(switchKey))
+        {
+            isFollowingCar = !isFollowingCar;
+        }
 
+        if (isFollowingCar)
+        {
+            // If in car-following mode, move the camera behind the car
+            FollowCar();
+        }
+        else
+        {
+            // Otherwise, control the player
+            ControlPlayer();
+        }
+    }
+
+    // Handles player control
+    private void ControlPlayer()
+    {
+        pitch = Mathf.Clamp(pitch - Input.GetAxis("Mouse Y") * _rotationSpeed, -90, 90);
         yaw += Input.GetAxis("Mouse X") * _rotationSpeed;
 
-       /* if (characterController.isGrounded)
-        {*/
-            // We are grounded, so recalculate
-            // move direction directly from axes
+        moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        float speed = _speed;
 
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
-            float speed = _speed;
+        if (Input.GetKey(_run)) 
+        {
+            speed = _runSpeed;
+        }
 
-            if (Input.GetKey(_run)) 
-            {
-                speed = _runSpeed;
-            }
+        moveDirection *= speed;
 
-            moveDirection *= speed;
-
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = _jumpSpeed;
-            }
+        if (Input.GetButton("Jump"))
+        {
+            moveDirection.y = _jumpSpeed;
+        }
         if (Input.GetKey(_flyDown))
         {
             moveDirection.y = -_jumpSpeed;
         }
 
         this.transform.localEulerAngles = new Vector3(0, yaw, 0);
-            
-            moveDirection = this.transform.TransformDirection(moveDirection);            
-        //}
-        
-        this.transform.localEulerAngles = new Vector3(0, yaw, 0);
+        moveDirection = this.transform.TransformDirection(moveDirection);
+
         _camera.transform.localEulerAngles = new Vector3(pitch, 0, 0);
 
-        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-        // as an acceleration (ms^-2)
+        // Apply gravity
         moveDirection.y -= _gravity * Time.deltaTime;
 
-        // Move the controller
+        // Move the player controller
         characterController.Move(moveDirection * Time.deltaTime);
-
-        
     }
+
+    // Handles following the car
+    // Handles following the car
+private void FollowCar()
+{
+    // Calculate the desired camera position based on the car's position and its forward direction
+    Vector3 desiredPosition = car.transform.position - car.transform.forward * -carCameraOffset.z + Vector3.up * carCameraOffset.y;
+
+    // Set the camera position to the desired position
+    _camera.transform.position = desiredPosition;
+
+    // Make the camera look at the car
+    _camera.transform.LookAt(car.transform);
+}
+
 }
